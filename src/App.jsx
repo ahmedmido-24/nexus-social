f9a4786e2066f31f5121b7b26c7d6934ee3b3216
ahmedmido-24 <ahmedmohamedmokhtarshalape2002@gmail.com>
2026-06-07 10:33:24 +0300

    fix: resolve case-sensitivity issue for ProtectedRoute and update README

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Profile from "./Components/Profile/Profile";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Notfound from "./Components/Notfound/Notfound";
-import ProtectedRoute from "./Components/ProtectedRoute/protectedRoute";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import UserContextProvider from "./Context/UserContext";
 import PostContextProvider from "./Context/PostContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import PostDetails from './Components/PostDetails/PostDetails';
+import PostDetails from "./Components/PostDetails/PostDetails";
 
 const queryClient = new QueryClient();
 
